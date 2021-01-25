035c7ae32c78864a935a88dff129f710a4f0d72d
Bashir Filipp <filipp218@gmail.com>
2021-01-25 21:31:50 +0300

    'initial'

--- yum/templatetags/main_menu.py
+++ yum/templatetags/main_menu.py
@@ -4,7 +4,28 @@ from yum.models import MenuItem
 
 register = template.Library()
 
+def into_tree(parent, children_by_parent): #функция, которая берёт родителя и смотрит  есть ли у него дети, если есть она идёт в глубь и спрашивает у следующего сына.
+    if parent in children_by_parent:
+        for m in children_by_parent[parent]:
+            m.children = children_by_parent[m]
+            into_tree(m, children_by_parent)
+    else:
+        return
+
 @register.inclusion_tag('yum/menu.html')
-def main_tree():
-    menu = MenuItem.objects.all()
-    return {'menu': menu}
+def main_tree(name):
+    menu = MenuItem.objects.filter(name = name)
+    children_by_parent = {}
+    for m in menu:
+        if m.parent not in children_by_parent:
+            children_by_parent[m.parent] = [m]
+        else:
+            children_by_parent[m.parent].append(m)
+
+    result = children_by_parent[None]
+    for m in result:
+        into_tree(m, children_by_parent)
+
+    return {'menu': result}
+    start_code = ['{% if item.children %}','<ul>','{% for child in item.children %}','<li> {{child}} </li>']
+    end_code = ['{% endfor %}','</ul>']
