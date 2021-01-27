7666103a706b3f4789965b10a957db0a9756d69d
Bashir Filipp <filipp218@gmail.com>
2021-01-27 22:49:36 +0300

    ready

--- yum/templatetags/main_menu.py
+++ yum/templatetags/main_menu.py
@@ -6,12 +6,13 @@ register = template.Library()
 
 def into_tree(parent, children_by_parent,code): #функция, которая берёт родителя и смотрит  есть ли у него дети, если есть она идёт в глубь и спрашивает у следующего сына.
     if parent in children_by_parent:
-        code += ['<ul>']
+        code.append('<ul>')
         for m in children_by_parent[parent]:
-            code += ['<li>', m.title]
+            code.append('<li>')
+            code.append(m.title)
             into_tree(m, children_by_parent, code)
-            code += ['</li>']
-        code += ['</ul>']
+            code.append('</li>')
+        code.append('</ul>')
     else:
         return
 
@@ -27,11 +28,13 @@ def main_tree(name):
             children_by_parent[m.parent].append(m)
 
     result = children_by_parent[None]
-    code = ['<ul>']
+    code = []
+    code.append('<ul>')
     for m in result:
-        code += ['<li>', m.title]
+        code.append('<li>')
+        code.append(m.title)
         into_tree(m, children_by_parent, code)
-        code +=  ['</li>']
-    code += ['</ul>']
+        code.append('</li>')
+    code.append('</ul>')
     p = {'a':code}
     return {'menu': p}
