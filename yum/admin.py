6371bf4997ca7a7e46451f004941fd5fa00a4e1a
Bashir Filipp <filipp218@gmail.com>
2021-01-31 12:17:25 +0300

    'final'

--- yum/admin.py
+++ yum/admin.py
@@ -2,5 +2,7 @@ from django.contrib import admin
 from .models import MenuItem
 # Register your models here.
 
+class MenuAdmin(admin.ModelAdmin):
+    list_display = ('title', 'url', 'name')
 
-admin.site.register(MenuItem)
+admin.site.register(MenuItem, MenuAdmin)
--- yum/templatetags/main_menu.py
+++ yum/templatetags/main_menu.py
@@ -1,40 +1,59 @@
 from django import template
 from yum.models import MenuItem
+from django.urls import reverse, NoReverseMatch
 
 
 register = template.Library()
 
 def into_tree(parent, children_by_parent,code): #функция, которая берёт родителя и смотрит  есть ли у него дети, если есть она идёт в глубь и спрашивает у следующего сына.
-    if parent in children_by_parent:
+    if parent.id in children_by_parent and parent.active is True:
         code.append('<ul>')
-        for m in children_by_parent[parent]:
+        for m in children_by_parent[parent.id]:
             code.append('<li>')
-            code.append(m.title)
+            code.append(f"<a href='{m.url}'>{m.title}</a>")
             into_tree(m, children_by_parent, code)
             code.append('</li>')
         code.append('</ul>')
     else:
         return
 
+def try_reverse(url):
+    try:
+        return reverse(url)
+    except NoReverseMatch:
+        return url
+
+
 
 @register.inclusion_tag('yum/menu.html')
-def main_tree(name):
+def draw_menu(name, active_path):
     menu = MenuItem.objects.filter(name = name)
     children_by_parent = {}
+    root_child = None
     for m in menu:
-        if m.parent not in children_by_parent:
-            children_by_parent[m.parent] = [m]
+        m.active = False
+        m.url = try_reverse(m.url)
+        if m.url == active_path:
+            m.active = True
+            root_child = m
+        if m.parent_id not in children_by_parent:
+            children_by_parent[m.parent_id] = [m]
         else:
-            children_by_parent[m.parent].append(m)
+            children_by_parent[m.parent_id].append(m)
 
-    result = children_by_parent[None]
-    code = []
-    code.append('<ul>')
-    for m in result:
-        code.append('<li>')
-        code.append(m.title)
-        into_tree(m, children_by_parent, code)
-        code.append('</li>')
-    code.append('</ul>')
-    p = {'a':code}
-    return {'menu': p}
+    if active_path != '/': #если юрл пустой
+        while root_child.parent_id != None:
+            for m in menu:
+                if root_child.parent_id == m.id:
+                    root_child = m
+                    root_child.active = True
+
+    result = []
+    result.append('<ul>')
+    for m in children_by_parent[None]:
+        result.append('<li>')
+        result.append(f"<a href='{m.url}'>{m.title}</a>")
+        into_tree(m, children_by_parent, result)
+        result.append('</li>')
+    result.append('</ul>')
+    return {'menu': result}
